777e424b9107f390804abdbb17d097c1991ed6e5
Mathew Daugherty <mathewdaugherty@mac.com>
2019-09-12 17:32:04 -0400

    added icon login buttons

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,19 +7,22 @@ export const LoginPage = ({ startGoogleLogin, startFbLogin }) => (
         <div className="box-layout__box">
             <h1 className="box-layout__title">Expensify</h1>
             <p>It's time to get your expenses under control.</p>
-            <button 
-                className="button"
-                onClick={startGoogleLogin}
-            >
-                Login with Google
-            </button>
+            <div>Login with:</div>
+            <div className="icons">
+                <button 
+                    className="button button--icon"
+                    onClick={startGoogleLogin}
+                >
+                    <img className="icon-buttons" src="/images/google-icon.png" title="Login with Google" />
+                </button>
 
-            <button 
-                className="button"
-                onClick={startFbLogin}
-            >
-                Login with Facebook
-            </button>
+                <button 
+                    className="button button--icon"
+                    onClick={startFbLogin}
+                >
+                    <img className="icon-buttons" src="/images/fb-icon.png" title="Login with Facebook" />
+                </button>
+            </div>
         </div>
         
     </div>
