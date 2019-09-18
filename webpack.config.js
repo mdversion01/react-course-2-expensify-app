181966da555735e8bc9ac64b388a274663cafa27
Mathew Daugherty <mathew.daugherty@deciphernow.com>
2019-09-18 10:12:27 -0400

    setting back to older webpack-dev-server until I figure out some issues

--- webpack.config.js
+++ webpack.config.js
@@ -55,17 +55,25 @@ module.exports = (env) => {
                 })
             }]
         },
+        // plugins: [
+        //     CSSExtract,
+        //     new webpack.DefinePlugin({
+        //         'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        //         'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        //         'process.env.FIREBASE_DATABASE': JSON.stringify(process.env.FIREBASE_DATABASE),
+        //         'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        //         'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_API_KEY),
+        //         'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+        //     })
+        // ],
         plugins: [
-            CSSExtract,
-            new webpack.DefinePlugin({
-                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-                'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-                'process.env.FIREBASE_DATABASE': JSON.stringify(process.env.FIREBASE_DATABASE),
-                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_API_KEY),
-                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-            })
-        ],
+           new webpack.LoaderOptionsPlugin({
+             // test: /\.xxx$/, // may apply this only for some modules
+             options: {
+               mode: ...
+             }
+           })
+         ],
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
